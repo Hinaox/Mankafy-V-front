0f880dad81115e367687f87c305f7eed86146ace
Hinaox <80781475+Hinaox@users.noreply.github.com>
2024-11-19 16:43:50 +0300

    chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
 })
 export class ChatComponent {
-
+  messages: { corps: string; sender: string }[] = [
+    { corps: 'Salut, comment-allez vous ?', sender: 'user' },
+    { corps: 'Ça va bien et vous ?', sender: 'bot' },
+    { corps: "Qu'est ce que je peux faire pour vous ?", sender: 'bot' },
+    {
+      corps: "J'aimerai demander votre avis concernant mon budget de vacances",
+      sender: 'user',
+    },
+    {
+      corps: 'Je peux vous aider à adapter vos vacances selon votre budget',
+      sender: 'bot',
+    },
+    { corps: "À combien s'élève votre budget ?", sender: 'bot' },
+    { corps: "Mon budget est de 5 millions d'ariary", sender: 'user' },
+    {
+      corps: 'Combien de personne est compris dans ce budget ?',
+      sender: 'bot',
+    },
+    { corps: 'Nous sommes un groupe de 4 personnes', sender: 'user' },
+  ];
 }
